65ced386b262e08aa3659746d1080dbeebfb39a8
derbestetv <uhlig.david01@gmail.com>
2025-09-28 21:24:33 +0200

    Refactor Inputmech.lua for improved readability and consistency in formatting

--- Inputmech.lua
+++ Inputmech.lua
@@ -1,10 +1,12 @@
-local rs, modem, eeprom = component.proxy(component.list("redstone")()), component.proxy(component.list("modem")()), component.proxy(component.list("eeprom")())
+local rs, modem, eeprom = component.proxy(component.list("redstone")()), component.proxy(component.list("modem")()),
+    component.proxy(component.list("eeprom")())
 local add, microType = eeprom.getLabel(), eeprom.getLabel():match("([^%s]+)")
-local ac, ma, DOWN, UP, SOUTH, EAST, WEST = {"", "", "", "", ""}, "", 0, 1, 3, 4, 5
-local activeColors = {[0] = "", [1] = "", [3] = "",  [4] = "",  [5] = ""  }
-local colorBits = {white, orange, magenta, lightBlue,yellow, lime, pink, gray,lightGray, cyan, purple, blue,brown, green, red, black}
+local ac, ma, DOWN, UP, SOUTH, EAST, WEST = { "", "", "", "", "" }, "", 0, 1, 3, 4, 5
+local activeColors = { [0] = "", [1] = "", [3] = "", [4] = "", [5] = "" }
+local colorBits = { white, orange, magenta, lightBlue, yellow, lime, pink, gray, lightGray, cyan, purple, blue, brown,
+    green, red, black }
 local colorNames = {}
-modem.broadcast(6000,add)
+modem.broadcast(6000, add)
 local start = 0
 function serialize(tbl)
     local function ser(val)
@@ -34,7 +36,6 @@ function unserialize(str)
     return f()
 end
 
-
 local function getColorStatusList()
     local input = rs.getBundledInput()[UP]
     local statusList = {}
@@ -44,11 +45,11 @@ local function getColorStatusList()
         else
             local active = false
         end
-        
+
         local cname = colorNames[color] or color
-        
+
         local symbol = active and "-" or "+"
-        table.insert(statusList, {name = cname, status = symbol})
+        table.insert(statusList, { name = cname, status = symbol })
     end
     return statusList
 end
@@ -57,51 +58,44 @@ end
 
 
 
-local function red()
-    if start == 1 then
-act = getColorStatusList()
- local changed = {}
+local function red(start1)
+    if start1 == 1 then
+        act = getColorStatusList()
+        local changed = {}
         for i, entry in ipairs(act) do
             local name = entry.name
             local status = entry.status
             if lastStatus[name] ~= status then
-                modem.broadcast(1, serialize({addr="fs", wert={name=status}}))
+                modem.broadcast(1, serialize({ addr = "fs", wert = { name = status } }))
                 lastStatus[name] = status
             end
         end
-        
-end
+    end
 end
 
 
-modem.open(2) 
+modem.open(2)
 --modem.open(1234)
 
 while true do
-  local e, _, from, port, _, m = computer.pullSignal()
+    local e, _, from, port, _, m = computer.pullSignal()
 
-  if e == "modem_message" then
- 
-    if port == 2 then
-  
-      message = unserialize(m)
+    if e == "modem_message" then
+        if port == 2 then
+            message = unserialize(m)
 
-if message.addr == add then
-
-    if message.wert == "start" then
-        modem.broadcast(6000,serialize(colorNames))
-        start = 1
-        red()
-    modem.broadcast(2,serialize({addr="fs", wert="start"}))
-    else
-        table.insert(colorNames, message.wert)
-        
+            if message.addr == add then
+                if message.wert == "start" then
+                    modem.broadcast(6000, serialize(colorNames))
+                    start = 1
+                    red(start)
+                    modem.broadcast(2, serialize({ addr = "fs", wert = "start" }))
+                else
+                    table.insert(colorNames, message.wert)
+                end
+            end
+        end
+    elseif e == "redstone" then
+        red(start)
     end
-
-   
-end
-    end 
-  elseif e == "redstone" then
-    red()
-  end
 end
