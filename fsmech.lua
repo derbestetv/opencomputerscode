f7e0e1331f0f3bf3dd735cec5aff4b5c8ef3075d
derbestetv <uhlig.david01@gmail.com>
2025-10-05 19:30:53 +0200

    sdfsdfsdf

--- fsmech.lua
+++ fsmech.lua
@@ -4,6 +4,7 @@ local add, microType = eeprom.getLabel(), eeprom.getLabel():match("([^%s]+)")
 local ac, ma, DOWN, UP, SOUTH, EAST, WEST = { "", "", "", "", "" }, "", 0, 1, 3, 4, 5
 local activeColors = { [0] = "", [1] = "", [3] = "", [4] = "", [5] = "" }
 local fs = {}
+local stat = {}
 modem.broadcast(6000, add)
 local start = 0
 function serialize(tbl)
@@ -45,6 +46,7 @@ modem.open(1)
 while true do
     local e, _, from, port, _, m = computer.pullSignal()
 
+    
     if e == "modem_message" then
         if port == 2 then
             message = unserialize(m)
@@ -56,12 +58,27 @@ while true do
                 
             elseif message.addr =="all" then
                 if message.wert == "start" then
-                    modem.broadcast(6001, serialize(fs))
+                    --modem.broadcast(6001, serialize(fs))
                     start = 1
                     
                     modem.broadcast(2, serialize({ addr = "fs", wert = "start" }))
                 end
             end
+        else
+            --{1 = {F1={...}},2={F2={...}},...}
+            --{F1={...}}
+                stat[message.name] = message.wert
+            for i, fs1 in ipairs(fs) do
+                for key1,stellall in pairs(fs1) do
+                    for key,stell in pairs(stellall) do
+                        if stat[key] == stell then
+                        modem.broadcast(6001, key1)
+                        end
+                    end
+                    
+                end
+             end
+            
         end
     end
 
