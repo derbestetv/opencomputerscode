95a0259b1915ef167973461d6c9983f6b9f49b65
derbestetv <uhlig.david01@gmail.com>
2025-10-05 21:24:42 +0200

    dfsdf

--- fsmech.lua
+++ fsmech.lua
@@ -1,4 +1,4 @@
-local modem, eeprom =  component.proxy(component.list("modem")()),
+local modem, eeprom = component.proxy(component.list("modem")()),
     component.proxy(component.list("eeprom")())
 local add, microType = eeprom.getLabel(), eeprom.getLabel():match("([^%s]+)")
 local ac, ma, DOWN, UP, SOUTH, EAST, WEST = { "", "", "", "", "" }, "", 0, 1, 3, 4, 5
@@ -27,6 +27,7 @@ function serialize(tbl)
     end
     return ser(tbl)
 end
+
 -- Deserialisierung: String → Table
 function unserialize(str)
     local f, err = load("return " .. str, nil, "t", {})
@@ -34,33 +35,23 @@ function unserialize(str)
     return f()
 end
 
-
-
-
-
-
-
-
 modem.open(2)
 modem.open(1)
 while true do
     local e, _, from, port, _, m = computer.pullSignal()
 
-    
+
     if e == "modem_message" then
         if port == 2 then
             message = unserialize(m)
 
             if message.addr == add then
-             
-                    table.insert(fs, message.wert)
-                    
-                
-            elseif message.addr =="all" then
+                table.insert(fs, message.wert)
+            elseif message.addr == "all" then
                 if message.wert == "start" then
-                    --modem.broadcast(6001, serialize(fs))
+                    modem.broadcast(6001, serialize(fs))
                     start = 1
-                    
+
                     modem.broadcast(2, serialize({ addr = "fs", wert = "start" }))
                 end
             end
@@ -68,19 +59,28 @@ while true do
             --{1 = {F1={...}},2={F2={...}},...}
             --{F1={...}}
             message = unserialize(m)
-                stat[message.name] = message.wert
-            for i, fs1 in ipairs(fs) do
-                for key1,stellall in pairs(fs1) do
-                    for key,stell in pairs(stellall) do
-                        if stat[key] == stell then
-                        modem.broadcast(6001, key1)
+            stat[message.name] = message.wert
+            for i, fs_entry in ipairs(fs) do
+                -- fs_entry ist: {F1 = {W11 = "-", W10 = "+", ...}}
+                for fahrweg_name, stell_all in pairs(fs_entry) do
+                    -- fahrweg_name ist: "F1"
+                    -- stell_all ist: {W11 = "-", W10 = "+", ...}
+
+                    if type(stell_all) == "table" then
+                        local all_match = true
+                        for signal_name, required_value in pairs(stell_all) do
+                            if stat[signal_name] ~= required_value then
+                                all_match = false
+                                break
+                            end
+                        end
+                        if all_match then
+                            modem.broadcast(6001, fahrweg_name)
+                            modem.broadcast(6000, "TREFFER: " .. fahrweg_name)
                         end
                     end
-                    
                 end
-             end
-            
+            end
         end
     end
-
 end
